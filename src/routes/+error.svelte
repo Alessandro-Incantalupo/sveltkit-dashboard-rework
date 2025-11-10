c51480030b25417ce8d1641e37acac21951f6e28
Alessandro Mauro Incantalupo <alessandro.incantalupo@gmail.com>
2025-11-10 17:53:29 +0100

    refactor: used centralized tailwind vars

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -9,7 +9,7 @@
 		<p class="text-gray mb-4">The resource you're looking for doesn't exist or has been removed.</p>
 		<a
 			href="/resources"
-			class="inline-flex items-center gap-2 rounded-lg bg-orange-500 px-4 py-2 text-white hover:bg-orange-600"
+			class="inline-flex items-center gap-2 rounded-lg bg-brand px-4 py-2 text-white hover:bg-brand-dark"
 		>
 			<Icon icon="mdi:arrow-left" class="h-4 w-4" />
 			Back to Resources
